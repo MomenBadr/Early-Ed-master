567606bc9b832c846d004dbb56c7ad08f0989c99
Momen Badr <momenamer666@gmail.com>
2024-05-06 15:02:23 +0300

    Initial commit

---
+++ lib/screens/attendance_screen.dart
@@ -0,0 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:early_ed/database/my_database.dart';
+import 'package:early_ed/widgets/attendance_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jiffy/jiffy.dart';
+
+class AttendanceScreen extends StatefulWidget {
+  const AttendanceScreen({super.key, this.studentId, required this.type});
+  final studentId;
+  final String type;
+
+  @override
+  State<AttendanceScreen> createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(25),
+                bottomLeft: Radius.circular(25)),
+          ),
+          title: Column(
+            children: [
+              Image.asset(
+                "assets/images/EARLYED.png",
+                width: 150,
+                height: 100,
+                fit: BoxFit.fill,
+              ),
+            ],
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.lightBlue,
+          actions: const [
+            Icon(Icons.more_vert_rounded, size: 33, color: Colors.black)
+          ],
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(17),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    child: const Text(
+                      "Attendance",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 20.h),
+                  Expanded(
+                      child: StreamBuilder<DocumentSnapshot>(
+                        stream: firestore
+                            .collection('userslist')
+                            .doc(widget.studentId ?? auth.currentUser!.uid)
+                            .snapshots(),
+                        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                          /*if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const Center(child: CircularProgressIndicator());
+                          }*/
+                          if (snapshot.hasError) {
+                            return Center(child: Text('Error: ${snapshot.error}'));
+                          }
+                          if (!snapshot.hasData || !snapshot.data!.exists) {
+                            return const Center(child: Text('No data found'));
+                          }
+
+                          var data = snapshot.data!;
+                          return ListView.builder(
+                            itemBuilder: (buildContext, index) {
+                              return AttendanceData(
+                                day: Jiffy.now().subtract(days: data['weekAtt'].length - 1 - index).E,
+                                date: Jiffy.now().subtract(days: data['weekAtt'].length - 1 - index).MMMd,
+                                present: data['weekAtt'][index] == '1',
+                                attendance: data['weekAtt'].map((item) => item.toString()).toList(),
+                                index: index, studentId: widget.studentId,
+                                type: widget.type,
+                              );
+                            },
+                            itemCount: data['weekAtt'].length,
+                          );
+                        },
+                      )
+                  )])));
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,277 @@
+import 'dart:developer';
+import 'package:early_ed/database/my_database.dart';
+import 'package:early_ed/database/user_data_provider.dart';
+import 'package:early_ed/screens/School_info.dart';
+import 'package:early_ed/screens/Student_info.dart';
+import 'package:early_ed/screens/attendance_screen.dart';
+import 'package:early_ed/screens/grades_screen.dart';
+import 'package:early_ed/screens/news/news_screen.dart';
+import 'package:early_ed/screens/select_level_screen.dart';
+import 'package:early_ed/screens/user_chats/user_chats.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  static const String routeName = 'Home Screen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String userName = '',
+      userEmail = '',
+      userId = '',
+      parentOrChild = '',
+      password = '',
+      subject = '',
+      type = '',
+      userImageUrl = '';
+  int age = 0, level = 0;
+  List<String> mathGrades = [],
+      englishGrades = [],
+      scienceGrades = [],
+      arabicGrades = [],
+      weekAtt = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  // استرجاع البيانات المحفوظة عند تحميل الصفحة
+  void loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userName = prefs.getString('userName') ?? '';
+      userEmail = prefs.getString('userEmail') ?? '';
+      userId = prefs.getString('userId') ?? '';
+      parentOrChild = prefs.getString('parentOrChild') ?? '';
+      password = prefs.getString('password') ?? '';
+      subject = prefs.getString('subject') ?? '';
+      type = prefs.getString('type') ?? '';
+      userImageUrl = prefs.getString('userImageUrl') ?? '';
+      age = prefs.getInt('age') ?? 0;
+      level = prefs.getInt('level') ?? 0;
+      mathGrades = prefs.getStringList('mathGrades') ?? [];
+      arabicGrades = prefs.getStringList('arabicGrades') ?? [];
+      scienceGrades = prefs.getStringList('scienceGrades') ?? [];
+      englishGrades = prefs.getStringList('englishGrades') ?? [];
+      weekAtt = prefs.getStringList('weekAtt') ?? [];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //final size =MediaQuery.of(context).size;
+    var userDataProvider = Provider.of<UserDataProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home, size: 35),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+        ),
+        title: Text(
+          "EARLYED -- ${userName}",
+          style: const TextStyle(fontWeight: FontWeight.w900, fontSize: 13),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SchoolInfo(),
+                  ));
+            },
+            icon: const Icon(Icons.more_vert_rounded),
+            iconSize: 30,
+            color: Colors.black,
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => (type == 'ad' || type == 'te')
+                            ? const SelectLevelScreen(
+                                isGrades: true,
+                              )
+                            : GradesScreen(
+                                math: mathGrades,
+                                arabic: arabicGrades,
+                                english: englishGrades,
+                                science: scienceGrades,
+                                studentId: auth.currentUser!.uid, type: type, subject: subject
+                              )));
+              },
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                width: 200,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white12,
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  border: Border.all(color: Colors.blueAccent),
+                ),
+                child: const Text(
+                  "GRADES",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => (type == 'st' || type == 'pa')
+                          ? AttendanceScreen(studentId: auth.currentUser!.uid, type: type,)
+                          : const SelectLevelScreen(
+                              isGrades: false,
+                            ),
+                    ));
+              },
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                width: 200,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  border: Border.all(color: Colors.blueAccent),
+                ),
+                child: const Text(
+                  "ATTENDANCE",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          NewsScreen(canEdit: type == 'ad'),
+                    ));
+                log(type);
+              },
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                width: 200,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  border: Border.all(color: Colors.blueAccent),
+                ),
+                child: const Text(
+                  "NEWS",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => StudentInfo(
+                        userName: userName,
+                        age: age,
+                        imageUrl: userImageUrl, level: level,
+                      ),
+                    ));
+              },
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                width: 200,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  border: Border.all(color: Colors.blueAccent),
+                ),
+                child: const Text(
+                  "STUDENT INFO",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MyChat(),
+                    ));
+              },
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                width: 200,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  border: Border.all(color: Colors.blueAccent),
+                ),
+                child: const Text(
+                  "Chat",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+            ),
+            Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 40),
+                // padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                width: 310,
+                height: 210,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueAccent, width: 6),
+                    borderRadius: const BorderRadius.all(Radius.circular(30))),
+                child: Image.asset(
+                  "assets/images/student.jpg",
+                  fit: BoxFit.fitHeight,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
